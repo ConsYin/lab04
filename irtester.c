06aae89544d0645b19e6ab6057b03b9f221f716e
ConsYin <newgodforever@outlook.com>
2018-10-25 18:08:59 -0400

    Update irtester.c

--- irtester.c
+++ irtester.c
@@ -1,25 +1,28 @@
-LDFLAGS=-lwiringPi
-CC=gcc
-CFLAGS=-lWarn  -pedantic
+#include <stdio.h>
+#include <wiringPi.h>
 
-irtester:	irtester.o
-	$(CC) irtester.o -o irtester $(LDFLAGS)
-
-irtester.o:	irtester.c
-	$(CC) $(CFLAGS) -c -ansi $<
-
-tester: tester.o libmyifttt.a
-	$(CC) tester.o -L. -lmyifttt -lcurl -o tester
-
-libmyifttt.a:	ifttt.o
-	ar -rcs libmyifttt.a ifttt.o
-
-ifttt.o: 	ifttt.c ifttt.h
-	$(CC) $(CFLAGS) -c -ansi $<
-
-tester.o:	tester.c ifttt.h
-	$(CC) $(CFLAGS) -c -ansi $<
-
-clean:
-	rm tester *.o
-
+int main(int argc, char *argv[])
+{
+  int i;
+  wiringPiSetup () ;
+  pinMode(0, INPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  while(1) {
+    printf("Waiting for reset\n");
+    
+    while(digitalRead(0) == 0){
+    printf("Waiting for event\n");
+    digitalWrite(1, HIGH); delay(500);
+    digitalWrite(2, LOW);
+	}
+    
+    while(digitalRead(0) == 1){
+    printf("Alarm\n");
+    digitalWrite (2, HIGH); delay(500);
+    digitalWrite (1, LOW);
+  	}
+  }
+  /*NOTREACHED*/
+  return 0 ;
+}
